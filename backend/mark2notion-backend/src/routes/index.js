329cb79c24ac005723cfdc49e08cf281a4cf369d
Sumit Girwal <sumitgirwals@gmail.com>
2025-01-20 23:00:48 +0530

    Update the index js for sample routing

--- backend/mark2notion-backend/src/routes/index.js
+++ backend/mark2notion-backend/src/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
     res.json({ message: 'Welcome to the Mark2Notion backend!' });
 });
 
+
+router.get('/sample', (req, res) => {
+    res.json({ message: 'This is a sample route', "user_id": 123, "title": "Sample Title" });
+});
 module.exports = router;
